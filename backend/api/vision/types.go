e5c47759926ad3d2312bfab0e9c72feccb8982da
gaurondorian@gmail.com <gaurondorian@gmail.com>
2023-08-23 21:16:35 +0200

    :sparkles: Reformat struct

--- backend/api/vision/types.go
+++ backend/api/vision/types.go
@@ -19,57 +19,12 @@ type Requests struct {
 type VisionResult struct {
 	Responses []Responses `json:"responses"`
 }
-type Vertices struct {
-	X int `json:"x"`
-	Y int `json:"y"`
-}
-type BoundingPoly struct {
-	Vertices []Vertices `json:"vertices"`
-}
+
 type TextAnnotations struct {
-	Locale       string       `json:"locale,omitempty"`
-	Description  string       `json:"description"`
-	BoundingPoly BoundingPoly `json:"boundingPoly"`
+	Locale      string `json:"locale,omitempty"`
+	Description string `json:"description"`
 }
-type DetectedLanguages struct {
-	LanguageCode string  `json:"languageCode"`
-	Confidence   float64 `json:"confidence"`
-}
-type Property struct {
-	DetectedLanguages []DetectedLanguages `json:"detectedLanguages"`
-}
-type BoundingBox struct {
-	Vertices []Vertices `json:"vertices"`
-}
-type Symbols struct {
-	BoundingBox BoundingBox `json:"boundingBox"`
-	Text        string      `json:"text"`
-}
-type Words struct {
-	Property    Property    `json:"property"`
-	BoundingBox BoundingBox `json:"boundingBox"`
-	Symbols     []Symbols   `json:"symbols"`
-}
-type Paragraphs struct {
-	BoundingBox BoundingBox `json:"boundingBox"`
-	Words       []Words     `json:"words"`
-}
-type Blocks struct {
-	BoundingBox BoundingBox  `json:"boundingBox"`
-	Paragraphs  []Paragraphs `json:"paragraphs"`
-	BlockType   string       `json:"blockType"`
-}
-type Pages struct {
-	Property Property `json:"property"`
-	Width    int      `json:"width"`
-	Height   int      `json:"height"`
-	Blocks   []Blocks `json:"blocks"`
-}
-type FullTextAnnotation struct {
-	Pages []Pages `json:"pages"`
-	Text  string  `json:"text"`
-}
+
 type Responses struct {
-	TextAnnotations    []TextAnnotations  `json:"textAnnotations"`
-	FullTextAnnotation FullTextAnnotation `json:"fullTextAnnotation"`
+	TextAnnotations []TextAnnotations `json:"textAnnotations"`
 }
