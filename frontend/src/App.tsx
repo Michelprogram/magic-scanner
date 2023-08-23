cc877be2cf010670a867e3c2c4fa720ff48edd3c
gaurondorian@gmail.com <gaurondorian@gmail.com>
2023-08-23 23:41:56 +0200

    :spakles: PWA

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,13 +3,22 @@ import Video from "./components/video";
 import { useCard } from "./context/Cards";
 import { Button } from "./components/ui/button";
 import { Toaster } from "./components/ui/toaster";
+import { ThemeProvider } from "./components/ui/theme-provider";
+import { Switch } from "./components/ui/switch";
+import { Label } from "./components/ui/label";
 
 function App() {
   const video = useRef<HTMLVideoElement>(null);
 
   const [selected, setSelected] = useState(false);
 
+  const [sw, setSwitch] = useState("EN");
+
   const { FetchCards, GetLoading } = useCard();
+
+  const changeSwitch = () => {
+    setSwitch(sw == "FR" ? "EN" : "FR");
+  };
 
   const screenshot = () => {
     const CROP = 300;
@@ -38,20 +47,26 @@ function App() {
   }, [GetLoading]);
 
   return (
-    <div className=" flex w-3/4 m-auto gap-6 flex-col items-center pt-10">
-      <h1 className="text-3xl font-bold text-center">
-        Magic the gathering scanner
-      </h1>
-      <div className=" h-fit">
-        <Video video={video} />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="flex w-3/4 m-auto gap-6 flex-col items-center pt-10">
+        <h1 className="text-3xl font-bold text-center">
+          Magic the gathering scanner
+        </h1>
+        <div className="h-fit">
+          <Video video={video} />
+        </div>
+        <div className="flex flex-col items-center gap-5">
+          <div className="flex items-center gap-2">
+            <Switch id="language-mode" onClick={changeSwitch} />
+            <Label htmlFor="language-mode">{sw}</Label>
+          </div>
+          <Button onClick={screenshot} disabled={selected}>
+            Flash the card
+          </Button>
+        </div>
+        <Toaster />
       </div>
-      <div className=" flex flex-col">
-        <Button onClick={screenshot} disabled={selected}>
-          Flash the card
-        </Button>
-      </div>
-      <Toaster />
-    </div>
+    </ThemeProvider>
   );
 }
 
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import basicSsl from "@vitejs/plugin-basic-ssl";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default ({ mode }) => {
   process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ""));
@@ -9,8 +10,53 @@ export default ({ mode }) => {
     plugins: [react()],
   });
 
+  const assets = [
+    "favicon.ico",
+    "apple-touch-icon-180x180.png",
+    "maskable-icon-512x512.png",
+  ];
+
+  const manifest = {
+    name: "Scan magic the gathering",
+    short_name: "Scan mgtr",
+    description: "Scan magic the gathering",
+    theme_color: "#ffffff",
+    icons: [
+      {
+        src: "pwa-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "pwa-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
+        src: "pwa-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any",
+      },
+      {
+        src: "pwa-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable",
+      },
+    ],
+  };
+
   if (process.env.APP_ENV == "MOBILE") {
     conf.plugins.push(basicSsl());
+    conf.plugins.push(
+      VitePWA({
+        registerType: "autoUpdate",
+        devOptions: { enabled: true },
+        manifest,
+        includeAssets: assets,
+      })
+    );
     conf.server = {
       https: true,
       proxy: {
@@ -20,6 +66,14 @@ export default ({ mode }) => {
       },
     };
   } else if (process.env.APP_ENV == "DEV") {
+    conf.plugins.push(
+      VitePWA({
+        registerType: "autoUpdate",
+        devOptions: { enabled: true },
+        manifest,
+        includeAssets: assets,
+      })
+    );
     conf.server = {
       proxy: {
         "/v1": {
